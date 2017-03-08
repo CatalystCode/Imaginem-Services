1c25806ea3ce62e469bc08eed59111b5e3641233
Erik Schlegel <erisch@microsoft.com>
2017-03-08 16:56:08 -0600

    changing table deployment.

--- ddl/createImaginemDb.sql
+++ ddl/createImaginemDb.sql
@@ -1,27 +1,27 @@
 ﻿IF OBJECT_ID('pipeline_output') IS NOT NULL
  BEGIN
-	USE [imaginem]
+	USE imaginem
 	GO
 
-	CREATE TABLE [dbo].[pipeline_output](
-		[id] [nvarchar](max) NULL,
-		[processing_pipeline] [ntext] NULL,
-		[image_url] [nvarchar](max) NULL,
-		[image_parameters] [ntext] NULL,
-		[job_output] [ntext] NULL,
-		[timestamp] [datetime] NULL,
-		[batch_id] [varchar](100) NULL,
-		[categoriesJSON]  AS (json_query(rtrim(CONVERT([nvarchar](max),[job_output])),'$.generalclassification.categories')),
-		[tagsJSON]  AS (json_query(rtrim(CONVERT([nvarchar](max),[job_output])),'$.generalclassification.tags')),
-		[descriptionJSON]  AS (json_query(rtrim(CONVERT([nvarchar](max),[job_output])),'$.generalclassification.description.tags'))
+	CREATE TABLE dbo.pipeline_output(
+		id varchar(50) NULL,
+		processing_pipeline ntext NULL,
+		image_url nvarchar(max) NULL,
+		image_parameters ntext NULL,
+		job_output ntext NULL,
+		timestamp datetime NULL,
+		batch_id varchar(100) NULL,
+		categoriesJSON  AS (json_query(rtrim(CONVERT(nvarchar(max),job_output)),'$.generalclassification.categories')),
+		tagsJSON  AS (json_query(rtrim(CONVERT(nvarchar(max),job_output)),'$.generalclassification.tags')),
+		descriptionJSON  AS (json_query(rtrim(CONVERT(nvarchar(max),job_output)),'$.generalclassification.description.tags'))
 	GO
 
-	CREATE NONCLUSTERED INDEX [IX_pipeline_output] ON [dbo].[pipeline_output]
+	CREATE NONCLUSTERED INDEX IX_pipeline_output ON dbo.pipeline_output
 	(
-		[timestamp] ASC
+		timestamp ASC
 	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 	GO
 
-	ALTER TABLE [dbo].[pipeline_output]
+	ALTER TABLE dbo.pipeline_output
 	ADD CONSTRAINT PK_pipeline_output_ID PRIMARY KEY CLUSTERED (ID);  
  END
